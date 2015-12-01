b0b62b154022a8f70a8e871009baa46d37ee7892
wjchen <wjchen0@gmail.com>
2015-12-01 13:44:50 +0800

    update  method to get share info update http request

--- baidutransfer.go
+++ baidutransfer.go
@@ -17,14 +17,21 @@ import (
 	"time"
 )
 
-var uid = 1075874930
-var cookie = ""
-var savepath = "/book"
+var (
+	uid      = 1075874930
+	cookie   = ""
+	savepath = "/book"
+)
 
-const post_fmt = "http://yun.baidu.com/share/transfer?shareid=%s&from=%s&bdstoken=%s&channel=chunlei&clienttype=0&web=1&app_id=250528" //suguliang
-const user_agent_iphone = "Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5"
-const user_agent_chrome = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36"
-const list_fmt = "http://yun.baidu.com/share/homerecord?uk=%d&page=%d&pagelength=60" //suguliang
+const (
+	post_fmt          = "http://yun.baidu.com/share/transfer?shareid=%s&from=%s&bdstoken=%s&channel=chunlei&clienttype=0&web=1&app_id=250528" //suguliang
+	user_agent_iphone = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25"
+	user_agent_chrome = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
+	list_fmt          = "http://yun.baidu.com/share/homerecord?uk=%d&page=%d&pagelength=60"
+
+	//err
+	ErrnoExist = 12
+)
 
 type Items struct {
 	Errno int    `json:"errno"`
@@ -87,9 +94,12 @@ Loop:
 			h := t.Hour()
 			min := t.Minute()
 			when := fmt.Sprintf("%d-%02d-%02d %02d:%02d", y, m, d, h, min)
-			url_str := fmt.Sprintf("http://yun.baidu.com/s/%s", items.List[j].Shorturl)
-			baiduTransfer(url_str, items.List[j].TypicalPath, savepath)
 			fmt.Printf("%s\t%s\t%s\r\n", items.List[j].Shorturl, when, items.List[j].TypicalPath)
+
+			url_str := fmt.Sprintf("http://yun.baidu.com/s/%s", items.List[j].Shorturl)
+			if err := baiduTransfer(url_str, items.List[j].TypicalPath, savepath); err != nil {
+				break Loop
+			}
 			fmt.Println("======================================================================")
 		}
 		if n == 0 {
@@ -99,11 +109,17 @@ Loop:
 	}
 }
 
-func baiduTransfer(url_str, file, path string) {
+func baiduTransfer(url_str, file, path string) error {
+	time.Sleep(time.Second * 1)
 	body, err := HttpRequest("GET", url_str, map[string]string{
+		"Host":            "pan.baidu.com",
 		"User-Agent":      user_agent_iphone,
 		"Cookie":          cookie,
+		"Connection":      "keep-alive",
+		"Accept":          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-CN,zh;q=0.8",
+		"DNT":             "1",
 	}, nil)
 	checkError(err)
 
@@ -112,38 +128,62 @@ func baiduTransfer(url_str, file, path string) {
 	time.Sleep(time.Second * 1)
 	filelist := fmt.Sprintf("[\"%s\"]", file)
 	body2, err := HttpRequest("POST", url_post, map[string]string{
-		"User-Agent": user_agent_chrome,
-		"Cookie":     cookie,
-		"Referer":    url_str,
+		"User-Agent":       user_agent_chrome,
+		"Cookie":           cookie,
+		"Referer":          url_str,
+		"Origin":           "http://yun.baidu.com",
+		"X-Requested-With": "XMLHttpRequest",
+		"Accept":           "*/*",
+		"Accept-Language":  "zh-CN,zh;q=0.8",
+		"Connection":       "keep-alive",
+		"Content-Type":     "application/x-www-form-urlencoded; charset=UTF-8",
 	}, map[string]string{
 		"filelist": filelist,
 		"path":     path,
 	})
 	checkError(err)
 	fmt.Println(string(body2))
+	resp := struct {
+		Errno   int         `json:"errno"`
+		Task_id interface{} `json:"task_id"`
+		Info    interface{} `json:"info"`
+	}{}
+	err = json.Unmarshal(body2, &resp)
+	checkError(err)
+	if resp.Errno == ErrnoExist { //stop when file exits
+		return errors.New("need stop")
+	}
+	return nil
 }
 
-const MAX_BODY_SIZE = 32 * 1024 * 1024 //32M
+var cookieJar = &myCookieJar{}
+
+const MAX_BODY_SIZE = 8 * 1024 * 1024 //2M
 func HttpRequest(method, url_str string, head_map, form_map interface{}) ([]byte, error) {
 	buffer := bytes.NewBuffer(nil)
 	url_obj, err := url.Parse(url_str)
 	if err != nil {
 		return buffer.Bytes(), errors.New("Url format error")
 	}
-	client := &http.Client{}
-	form_value := url.Values{}
+	client := &http.Client{Jar: cookieJar}
+	var dataBody io.Reader
 	if form_map != nil {
 		switch v := form_map.(type) {
 		case map[string]string:
+			form_value := url.Values{}
 			for key, value := range v {
 				form_value.Set(key, value)
 			}
+			dataBody = strings.NewReader(form_value.Encode())
+		case string:
+			dataBody = strings.NewReader(v)
+		case []byte:
+			dataBody = bytes.NewReader(v)
 		default:
 			return buffer.Bytes(), errors.New("The form arg is not a map")
 		}
 	}
-
-	request, err := http.NewRequest(method, url_obj.String(), strings.NewReader(form_value.Encode()))
+	request, err := http.NewRequest(method, url_obj.String(), dataBody)
 	if err != nil {
 		return buffer.Bytes(), errors.New("HTTP GET failed")
 	}
@@ -158,8 +198,8 @@ func HttpRequest(method, url_str string, head_map, form_map interface{}) ([]byte
 			return buffer.Bytes(), errors.New("The head arg is not a map")
 		}
 	}
-	if method == "POST" {
-		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	for _, c := range cookieJar.cookies {
+		request.AddCookie(c)
 	}
 
 	response, err := client.Do(request)
@@ -172,7 +212,7 @@ func HttpRequest(method, url_str string, head_map, form_map interface{}) ([]byte
 	}
 
 	buf := make([]byte, 4096)
-	var rd interface{}
+	var rd io.Reader
 	switch response.Header.Get("Content-Encoding") {
 	case "gzip":
 		var err error
@@ -187,16 +227,7 @@ func HttpRequest(method, url_str string, head_map, form_map interface{}) ([]byte
 	}
 
 	for {
-		var n int
-		var err error
-		switch v := rd.(type) {
-		case *gzip.Reader:
-			n, err = v.Read(buf)
-		case io.ReadCloser:
-			n, err = v.Read(buf)
-		case *bufio.Reader:
-			n, err = v.Read(buf)
-		}
+		n, err := rd.Read(buf)
 		if err != nil && err != io.EOF {
 			return buffer.Bytes(), errors.New("HTTP body read error")
 		}
@@ -205,26 +236,67 @@ func HttpRequest(method, url_str string, head_map, form_map interface{}) ([]byte
 		}
 		buffer.Write(buf[:n])
 		if buffer.Len() > MAX_BODY_SIZE {
-			return buffer.Bytes(), errors.New("HTTP body too large, partial read")
+			return buffer.Bytes(), errors.New("HTTP body too larg, partial readed")
 		}
 	}
 
 	return buffer.Bytes(), nil
 }
 
+func multipartForm(form_map map[string]string, boundary string) []byte {
+	buf := bytes.Buffer{}
+	crlf := []byte{0x0d, 0x0a}
+	disFmt := `Content-Disposition: form-data; name="%s"`
+	for key, value := range form_map {
+		buf.WriteString("--")
+		buf.WriteString(boundary)
+		buf.Write(crlf)
+		keyStr := fmt.Sprintf(disFmt, key)
+		buf.WriteString(keyStr)
+		buf.Write(crlf)
+		buf.Write(crlf)
+		buf.WriteString(value)
+		buf.Write(crlf)
+	}
+
+	buf.WriteString("--")
+	buf.WriteString(boundary)
+	buf.WriteString("--") //end
+	buf.Write(crlf)
+	return buf.Bytes()
+}
+
+type myCookieJar struct {
+	cookies []*http.Cookie
+}
+
+func (c *myCookieJar) SetCookies(u *url.URL, cookies []*http.Cookie) {
+	if c.cookies == nil {
+		c.cookies = make([]*http.Cookie, 0)
+	}
+
+	for _, it := range cookies {
+		c.cookies = append(c.cookies, it)
+	}
+}
+
+func (c *myCookieJar) Cookies(u *url.URL) []*http.Cookie {
+	return c.cookies
+}
+
 func GetInfo(body string) (shareid, uk, token string) {
-	re_shareid := regexp.MustCompile("FileUtils.shareid=\"([0-9].*?)\"")
+	re_shareid := regexp.MustCompile(`yunData.SHARE_ID\s*=\s*"([0-9].*?)"`)
 	array := re_shareid.FindStringSubmatch(body)
 	if len(array) >= 2 {
 		shareid = array[1]
 	}
 
-	re_uk := regexp.MustCompile("FileUtils.uk=\"([0-9].*?)\"")
+	re_uk := regexp.MustCompile(`yunData.MYUK\s*=\s*"([0-9].*?)"`)
 	array = re_uk.FindStringSubmatch(body)
 	if len(array) >= 2 {
 		uk = array[1]
 	}
-	re_token := regexp.MustCompile("FileUtils.bdstoken=\"([0-9a-z].*?)\"")
+	re_token := regexp.MustCompile(`yunData.MYBDSTOKEN\s*=\s*"([0-9a-z].*?)"`)
 	array = re_token.FindStringSubmatch(body)
 	if len(array) >= 2 {
 		token = array[1]
